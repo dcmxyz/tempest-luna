05d0cd27db35ec47ec9c36a7bdabb4e68f76bb0c
Daniel <daniel@wisk.ch>
2026-05-01 01:39:46 +0200

    feat(middleware): handle unused "remember me" cookies gracefully

--- app/Middleware/Authorize.php
+++ app/Middleware/Authorize.php
@@ -7,6 +7,7 @@ namespace App\Middleware;
 use App\Models\User;
 use App\Services\AuthService;
 use Tempest\Auth\Authentication\Authenticator;
+use Tempest\Http\Cookie\CookieManager;
 use Tempest\Http\Request;
 use Tempest\Http\Response;
 use Tempest\Router\HttpMiddleware;
@@ -17,6 +18,7 @@ final readonly class Authorize implements HttpMiddleware
     public function __construct(
         private Authenticator $authenticator,
         private AuthService $authService,
+        private CookieManager $cookieManager,
     ) {}
 
     public function __invoke(Request $request, HttpMiddlewareCallable $next): Response
@@ -47,14 +49,21 @@ final readonly class Authorize implements HttpMiddleware
             ->first();
 
         if ($user === null || $user->remember_token === null) {
+            $this->removeUnusedCookie();
             return;
         }
 
         if (! hash_equals($user->remember_token, hash('sha256', $validator))) {
+            $this->removeUnusedCookie();
             return;
         }
 
         $this->authenticator->authenticate($user);
         $this->authService->rotateRememberToken($user);
+    }
+
+    private function removeUnusedCookie(): void
+    {
+        $this->cookieManager->remove(AuthService::COOKIE_REMEMBER_TOKEN);
     }
 }
