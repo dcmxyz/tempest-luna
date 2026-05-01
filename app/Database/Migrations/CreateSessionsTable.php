197aa4c53e614dcb01448804cadc8c9880f506cb
Daniel <daniel@wisk.ch>
2026-05-01 23:17:02 +0200

    refactor(migrations): make CreateSessionsTable database-agnostic

--- app/Database/Migrations/CreateSessionsTable.php
+++ app/Database/Migrations/CreateSessionsTable.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Database\Migrations;
 
+use Tempest\Database\Config\DatabaseConfig;
+use Tempest\Database\Config\PostgresConfig;
 use Tempest\Database\MigratesUp;
 use Tempest\Database\QueryStatement;
 use Tempest\Database\QueryStatements\CreateTableStatement;
@@ -12,17 +14,30 @@ final class CreateSessionsTable implements MigratesUp
 {
     private(set) string $name = '0001-01-01_create_sessions_table';
 
+    public function __construct(
+        private readonly DatabaseConfig $databaseConfig,
+    ) {}
+
     public function up(): QueryStatement
     {
-        return new CreateTableStatement('sessions')
+        $createTableStatement = new CreateTableStatement('sessions')
             ->primary(uuid: true)
             ->datetime('created_at')
             ->datetime('last_active_at')
-            ->text('data')
-            ->raw('user_id UUID NULL') // Use Postgres native UUID type
+            ->text('data');
+
+        if ($this->databaseConfig instanceof PostgresConfig) {
+            $createTableStatement->raw(statement: "user_id UUID NULL");
+        } else {
+            $createTableStatement->string(name: 'user_id', length: 36, nullable: true);
+        }
+
+        $createTableStatement
             ->string('ip_address', length: 45, nullable: true)
             ->text('user_agent', nullable: true)
             ->index('user_id')
             ->index('last_active_at');
+
+        return $createTableStatement;
     }
 }
