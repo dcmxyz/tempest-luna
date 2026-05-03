3d4636a94c08e4df439ca4f6ae194c83eda72229
Daniel <daniel@wisk.ch>
2026-05-04 00:06:25 +0200

    feat(database): add support for MySQL and SQLite configurations

--- app/Config/database.config.php
+++ app/Config/database.config.php
@@ -2,14 +2,37 @@
 
 declare(strict_types=1);
 
+use Tempest\Database\Config\MysqlConfig;
 use Tempest\Database\Config\PostgresConfig;
+use Tempest\Database\Config\SQLiteConfig;
 
 use function Tempest\env;
+use function Tempest\root_path;
 
-return new PostgresConfig(
+$pgsqlConfig = new PostgresConfig(
     host: env('DATABASE_HOST', default: '127.0.0.1'),
     port: env('DATABASE_PORT', default: '5432'),
     username: env('DATABASE_USERNAME', default: 'postgres'),
-    password: env('DATABASE_PASSWORD', default: 'postgres'),
-    database: env('DATABASE_DATABASE', default: 'postgres'),
+    password: env('DATABASE_PASSWORD', default: ''),
+    database: env('DATABASE_DATABASE', default: 'luna'),
 );
+
+$mysqlConfig = new MysqlConfig(
+    host: env('DATABASE_HOST', default: '127.0.0.1'),
+    port: env('DATABASE_PORT', default: '3306'),
+    username: env('DATABASE_USERNAME', default: 'root'),
+    password: env('DATABASE_PASSWORD', default: ''),
+    database: env('DATABASE_DATABASE', default: 'luna'),
+);
+
+$sqliteConfig = new SQLiteConfig(
+    path: root_path(env('DATABASE_PATH', default: '.tempest/database.sqlite')),
+    persistent: env('DATABASE_PERSISTENT', default: true),
+);
+
+return match (env('DATABASE_CONFIG', default: 'sqlite')) {
+    'sqlite' => $sqliteConfig,
+    'pgsql' => $pgsqlConfig,
+    'mysql' => $mysqlConfig,
+    default => throw new InvalidArgumentException('Only the "sqlite", "pgsql" and "mysql" configs are supported.')
+};
